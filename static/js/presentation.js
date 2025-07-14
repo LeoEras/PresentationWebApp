e0b42081f27cc8f1451a93d4327fc529aff18af1
LeoEras <leoded94@hotmail.com>
2025-07-14 16:53:06 -0230

    Using Js to dynamically change the feedback provided by clicking an image

--- static/js/presentation.js
+++ static/js/presentation.js
@@ -1,10 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const thumbnails = document.querySelectorAll('.thumbnail-image');
-  const focusedImg = document.getElementById('focused-slide-img');
+document.addEventListener("DOMContentLoaded", function () {
+    const thumbnails = document.querySelectorAll(".thumbnail-image");
+    const focusedImg = document.getElementById("focused-slide-img");
+    const contrastSpan = document.getElementById("slide-contrast");
+    const wordsSpan = document.getElementById("slide-words");
+    const fontSizes = document.getElementById("slide-font-sizes");
+    const feedback = document.getElementById("slide-feedback-words");
 
-  thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('click', () => {
-      focusedImg.src = thumbnail.dataset.slideSrc;
+    thumbnails.forEach((thumb) => {
+      thumb.addEventListener("click", function () {
+        const newSrc = this.dataset.slideSrc;
+        const newContrast = this.dataset.contrast;
+        const newWords = this.dataset.words;
+        const feedbackContrastText = this.dataset.fontSize;
+        const feedbackWordsText = this.dataset.feedback;
+
+        focusedImg.src = newSrc;
+        contrastSpan.textContent = newContrast;
+        wordsSpan.textContent = newWords;
+        fontSizes.textContent = feedbackContrastText;
+        feedback.textContent = feedbackWordsText;
     });
   });
 });
